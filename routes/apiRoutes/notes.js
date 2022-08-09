78083f80fead4d993258cf8ebfb2901a42cebfaa
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-08-08 20:52:48 -0500

    code is written, but there is a routing error somewhere. program is non-functional.

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,18 +1,65 @@
 const router = require('express').Router();
-const { addNote, deleteNote } = require('../../lib/notes');
-let { notesArray } = require('../../db/notes');
+const { createNewNote, deleteNote } = require('../../lib/notes');
+let { notesArray } = require('../../db/db.json');
 
+// notes are available at api/notes in JSON 
 router.get('/notes', (req, res) => {
-    let results = notesArray;
-    res.json(results);
+  let results = notesArray;
+  res.json(results);
 });
 
 router.post('/notes', (req, res) => {
-    if(notesArray){
-        req.body.id = notesArray.length.toString();
-    } else 
-    {req.body.id = 0}
-    res.json(addNote(req.body, notesArray));
+  // set id based on what the next index of the array will be
+  if(notesArray){
+  req.body.id = notesArray.length.toString();
+  } else 
+  {req.body.id = 0}
+  res.json(createNewNote(req.body, notesArray));
 });
 
+// // Route parameters :👌 
+// router.delete('/notes/:id', async (req, res) => {
+//   const { id } = req.params
+//   notesArray = await deleteNote(id, notesArray);
+//   res.json(notesArray);
+// });
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const router = require('express').Router();
+// const { newNote } = require("../../lib/notes");
+// let { notesArray } = require('../../db/notes');
+
+// router.get('/notes', (req, res) => {
+//     let results = notesArray;
+//     res.json(results);
+// });
+
+// router.post('/notes', (req, res) => {
+//     if(notesArray){
+//         req.body.id = notesArray.length.toString();
+//     } else 
+//     {req.body.id = 0}
+//     res.json(newNote(req.body, notesArray));
+// });
+
+// module.exports = router;
+
+// module.exports = newNote;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
+const router = require('express').Router();
 
 // Parse JSON and URLs
 app.use(express.static('public'));
@@ -10,10 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// define paths for api and html routes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// // define paths for api and html routes
+// app.use("routes/", apiRoutes);
+// app.use("routes/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+module.export = router;
